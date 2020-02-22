611a560a8750e694ed2b7ce6ccca513846ed2d50
jilei.zheng <jilei.zheng@grabtaxi.com>
2020-02-22 16:19:35 +0800

    implement hello github actions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zhengjilei/github-actions-explore
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/zhengjilei/github-actions-explore/hello"
+)
+
+func main() {
+	fmt.Println(hello.Greet())
+}
